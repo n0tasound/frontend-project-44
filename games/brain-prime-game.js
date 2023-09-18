014ab449c59ff35f77e6c77798251e798748c908
Gleb Sharipov <inotasound@gmail.com>
2023-09-18 11:19:01 +0300

    fix brain-prime-game

--- games/brain-prime-game.js
+++ games/brain-prime-game.js
@@ -5,22 +5,22 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isNotPrime = (num, divider) => num % divider === 0;
 
-const primeCheck = (num) => {
+const isPrime = (num) => {
   const temp = (num ** (1 / 2));
   const divider = Math.trunc(temp);
 
   for (let i = 2; i <= divider; i += 1) {
     if (isNotPrime(num, i)) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const getQuestionAndAnswer = () => {
   const number = getRandomNum(3, 35);
   const question = `Question: ${number}`;
-  const rightAnswer = primeCheck(number);
+  const rightAnswer = isPrime(number) ? 'yes' : 'no';
 
   const result = [question, rightAnswer];
   return result;
